a6a4ed75104cf8d1599522eda0c2d350abb901eb
hari506 <sreehari.b6@gmail.com>
2024-05-27 06:33:26 +0530

    Delete UtilitiesImplemented directory

--- UtilitiesImplemented/routes/userRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const userContorller = require('./../controllers/userController');
-
-const router = express.Router();
-
-router.route('/').get(userContorller.getUsers);
-router.route('/:id').get(userContorller.getUserById);
-
-module.exports = router;
