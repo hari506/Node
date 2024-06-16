501fde3fbc6a0598a6ba0abce93c0e7bb425fd21
hari506 <sreehari.b6@gmail.com>
2024-06-16 17:55:19 +0530

    Delete controllers directory

--- controllers/billingController.js
+++
@@ -1,8 +0,0 @@
-const billingModel = require('./../models/billingModel');
-const factoryController = require('./factoryController');
-
-exports.getAllBillings = factoryController.getAll(billingModel);
-exports.insertBilling = factoryController.createOne(billingModel);
-exports.getBillingById = factoryController.getOne(billingModel);
-exports.updateBilling = factoryController.updateOne(billingModel);
-exports.deleteBilling = factoryController.deleteOne(billingModel);
